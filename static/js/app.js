e54f752574d6e6d046bebda54219baac868cc709
pbernhardt <papadiscobravo@users.noreply.github.com>
2021-04-13 11:37:35 -0500

    done

--- static/js/app.js
+++ static/js/app.js
@@ -1,104 +1,52 @@
-// This is app.js
+var tableData = data;
+console.log(tableData);
 
-console.log(`app.js has loaded`)
+var tbody = d3.select("tbody");
 
-var UFOSpotters = [
-    "John L.", "Milt", "Percy", "Connie",
-    "Florence", "Mary", "Diana", "Betty",
-    "Miles", "Wayne", "Herbie", "Ron", "Tony",
-    "Neal", "Gregg", "Michael S.", "Michael C.", "David Br.", "Carlos", "José",
-    "Maurice", "Philip", "Verdine", "Ralph",
-    "Ari", "Paloma", "Viv", "Tessa",
-    "Tina", "Chris F.", "David By.", "Jerry",
-    "Adam C.", "Larry Jr.", "Paul H.", "David E.",
-    "Belinda", "Jane", "Margot", "Elissa", 
-    "Bob", "Tom S.", "Paul W.", "Chris M.",
-    "Mike D.", "Adam Y.", "Adam H.",
-    "Thom Y.", "Jonny", "Colin", "Phil", "Ed" 
-    ];
-console.log(`There are ${UFOSpotters.length} UFO spotters in this practice dataset:`);
-
-// for (var i = 0 ; i < students.length; i++)
-    // console.log(students[i]);
-
-// for (var i = 0 ; i < UFOSpotters.length; i++)
-    // printName(UFOSpotters[i]);
-
-
-// This calls an anonymous or callback function:
-// UFOSpotters.forEach( function(name) {
-    // console.log(name);
-// });
-
-function printName(name) {
-    console.log(name);
+function buildTable(UFOData){
+    tbody.html("");
+    UFOData.forEach(element => {
+        // add rows
+        var row = tbody.append("tr");
+        // grab each column or key-value pair
+        Object.entries(element).forEach(function([key, value]){
+    
+            var cell = row.append("td");
+            // console.log(`${key}:${value}`)
+            cell.text(value);
+        });
+    });
+    
 }
+buildTable(tableData);
 
-// This calls the named function I just created:
-UFOSpotters.forEach(printName);
+// 1 Grab a handle on the input field. The id is called datetime.
 
-// An object in JS resembles a dictionary in Python.
-// It has a name and a series of key-value pairs:
-UFObject = {
-    datetime: "1/1/2010",
-    city: "benton",
-    state: "ar",
-    country: "us",
-    shape: "circle",
-    durationMinutes: "5 mins.",
-    comments: "4 bright green...my front door."
-  }
-// JSON stands for JavaScript Object Notation.
-// A JSON is a list of objects.
-// JS most commonly uses dot notation.
+// 2 Grab a handle on the Filter Table button. The id is filter-btn.
+var button = d3.select("#filter-btn");
 
-console.log(`On ${UFObject.datetime} in/near ${UFObject.city}, ${UFObject.state} ${UFObject.country}, a ${UFObject.shape} was reported for ${UFObject.durationMinutes} ${UFObject.comments}`);
+// 3 Install an event listener on the button.
+button.on("click", function(){
+    console.log(`button clicked`);
+    d3.event.preventDefault();
+    var inputField = d3.select("#datetime").node().value;
+    console.log(inputField);    
 
-// I can add a key-value pair to the object after the fact if I wish:
-randomSpotter = Math.floor(Math.random() * UFOSpotters.length)+1;
-// console.log(randomSpotter);
-UFObject.spotter = UFOSpotters[randomSpotter];
-console.log(`On ${UFObject.datetime} in/near ${UFObject.city}, ${UFObject.state} ${UFObject.country}, ${UFObject.spotter} reported seeing a ${UFObject.shape} for ${UFObject.durationMinutes} ${UFObject.comments}`);
+    // Build a filter that uses inputField to filter table data
+// and uses the table filtered data to rebuild the table in lines 6-16.
 
-// Similarly, I can delete a key-value pair from the object after the fact:
-delete UFObject.comments;
-console.log(UFObject);
+    var filteredData = tableData;
+    console.log(filteredData);
+    if (inputField != ""){
+        filteredData = filteredData.filter(function(row){
+            if(row.datetime === inputField){
+                return true;
+            }
+            else return false;
+        });    
+    }
+    console.log(filteredData);
 
-// I can check for a key-value pair in the JS object:
-if ("spotter" in UFObject) {
-    console.log(`This JS object has a spotter, ${UFObject.spotter}.`)
-}
+    buildTable(filteredData);
 
-// I can get the keys in an object:
-console.log(`${Object.keys(UFObject)}`);
-
-// I can get the key-value pairs in an object:
-console.log(`${Object.entries(UFObject)}`);
-
-// For-each doesn't return values across the = sign, but map does.
-// I'm going to use the array UFOSpotters to practice mapping.
-mapUFOSpotters = UFOSpotters.map(function(spotter) {
-    return spotter;
 });
-console.log(`Mapping over UFOSpotters returs this:\n${mapUFOSpotters}`);
-
-var mapUFOSpottersWithIndex = [];
-mapUFOSpottersWithIndex = mapUFOSpotters.map(function(item, index) {
-    return `{ name: "${item}", ID: ${index+1} }`;
-});
-
-mapUFOSpottersWithIndex.sort();
-console.log(`Sorting the mapped array with index returns this:\n${mapUFOSpottersWithIndex}`);
-
-var names = [];
-names = mapUFOSpottersWithIndex.map(function(spotter) {
-    return mapUFOSpottersWithIndex.name;
-});
-console.log(names);
-
-var IDs = [];
-IDs = mapUFOSpottersWithIndex.map(function(spotter) {
-    return mapUFOSpottersWithIndex.ID;
-});
-console.log(IDs);
-
